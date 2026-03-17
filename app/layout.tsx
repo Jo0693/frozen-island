26d1d1d62d21628a25dfcaff857a144350813716
Jo0693 <josselynmattei@gmail.com>
2026-03-17 12:59:45 +0800

    fix: replace test tracking IDs with real ones

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,21 +30,21 @@ export default function RootLayout({
         {/* Publisher injects tracking snippets (GTM, Clarity, etc.) here */}
       </head>
       <body className={`${baloo.variable} ${inter.variable}`} suppressHydrationWarning>
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TEST123" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PV899HZL" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
         {children}
         <Script id="gtm" strategy="afterInteractive">{`
   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-TEST123');
+  })(window,document,'script','dataLayer','GTM-PV899HZL');
 `}</Script>
         <Script id="clarity" strategy="afterInteractive">{`
   (function(c,l,a,r,i,t,y){
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-  })(window,document,"clarity","script","test123");
+  })(window,document,"clarity","script","vx0bhvi68d");
 `}</Script>
       </body>
     </html>
